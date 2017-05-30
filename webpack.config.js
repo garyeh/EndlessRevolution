537377889f703d55413d494820812307fed2c2e9
Gary Yeh <garyeh@gmail.com>
2017-05-30 11:37:15 -0700

    Copy drunken circles

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 
 module.exports = {
   context: __dirname,
-  entry: './lib/board.js',
+  entry: './lib/endless.js',
   output: {
     path: path.resolve(__dirname, 'lib'),
     filename: 'bundle.js',
