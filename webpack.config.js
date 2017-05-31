0a7e2e16e42a5d1c4e68ab5f7c820715bec83f91
Gary Yeh <garyeh@gmail.com>
2017-05-30 23:04:24 -0700

    Create scrolling arrows

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,7 @@
-var path = require('path');
-
 module.exports = {
-  context: __dirname,
-  entry: './lib/endless.js',
+  entry: "./lib/endless.js",
   output: {
-    path: path.resolve(__dirname, 'lib'),
-    filename: 'bundle.js',
-  },
-  module: {
-    loaders: [
-      {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      }
-    ]
+  	filename: "./lib/bundle.js"
   },
   devtool: 'source-map',
-  resolve: {
-    extensions: ['.js', '*']
-  }
 };
